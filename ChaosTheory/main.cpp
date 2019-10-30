233b72a3cacd389263e7c6ddd1fa31205b609e9e
MarioMigliacio <migliacio@gmail.com>
2019-10-29 20:42:21 -0700

    Dummy in main to test SFML window working.

--- ChaosTheory/main.cpp
+++ ChaosTheory/main.cpp
@@ -1,5 +1,20 @@
+#include <SFML/Graphics.hpp>
+
 int main()
 {
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Title");
+	sf::Event event;
 
-	return;
+	while(window.isOpen())
+	{
+		while(window.pollEvent(event))
+		{
+			if(event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+	}
+
+	return 0;
 }
