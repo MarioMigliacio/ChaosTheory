f4125b8199f304a5189495e409bec65cb96efce7
Mario <migliacio@gmail.com>
2025-07-04 13:31:44 -0700

    test push

--- src/core/ui/UIChatBox.cpp
+++ src/core/ui/UIChatBox.cpp
@@ -115,7 +115,7 @@ void UIChatBox::AddLine(const std::string &text)
     }
 }
 
-/// @brief Starts revealing the next line in the queue or clears text if queue is empty.
+/// @brief Starts revealing the next line in the queue or clears text if queue is empty. test
 void UIChatBox::StartNextLine()
 {
     if (m_nextIndicatorIcon)
