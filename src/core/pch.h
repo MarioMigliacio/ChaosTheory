432fcfea599a2e888a05c6c3b3801c013ed10c10
Mario <migliacio@gmail.com>
2025-07-30 20:32:04 -0700

    corrected file add. CmakeBuild with precompile header to 5x reduce build time.

---
+++ src/core/pch.h
@@ -0,0 +1,39 @@
+// ============================================================================
+//  File        : pch.h
+//  Project     : ChaosTheory (CT)
+//  Author      : Mario Migliacio
+//  Created     : 2025-04-11
+//  Description : Precompiled header to speed up compilation
+//
+//  License     : N/A Open source
+//                Copyright (c) 2025 Mario Migliacio
+// ============================================================================
+
+#pragma once
+
+// ==== Standard Library Includes ====
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+// ==== SFML ====
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+// ==== External Libraries ====
+#include <nlohmann/json.hpp>
+#include <spdlog/spdlog.h>
+
+// ==== Common Project Headers ====
+#include "Assets.h"
+#include "Macros.h"
+#include "ProjectilePresets.h"
+#include "Settings.h"
+#include "ShipPresets.h"
+#include "UIPresets.h"
